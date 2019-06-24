c40f551dac64f41e548fe72cd42b47efc81ca399
sarthak19-qait <sarthaksaxena@qainfotech.com>
2019-06-24 17:38:00 +0530

    push

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -8,14 +9,31 @@ export class AuthService {
 
   login(credentials) { 
    return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+      JSON.stringify(credentials)).map(response=>{
+        let result = response.json();
+          if(result && result.token){
+            localStorage.setItem('token',result.token)
+            return true
+          }
+          return false
+      });
   }
 
   logout() { 
+    localStorage.removeItem('token')
   }
 
   isLoggedIn() { 
-    return false;
+    return tokenNotExpired();           
+    //--------------------Above line is same as below----------------------------------
+    // let jwthelper = new JwtHelper();
+    // let token = localStorage.getItem('token');
+    // if(!token){
+    //   return false
+    // }
+    // let expirationDate = jwthelper.getTokenExpirationDate(token);
+    // let isExpired = jwthelper.isTokenExpired(token);
+    // return !isExpired;
   }
 }
 
